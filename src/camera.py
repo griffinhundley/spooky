508d3fb118a51418de58d9ab87495c017b835546
griffinhundley <hundlegq@gmail.com>
2021-10-22 15:56:48 -0400

    face cropping

--- src/camera.py
+++ src/camera.py
@@ -7,6 +7,12 @@ def get_frame():
     time.sleep(1)
     response, image = capture.read()
     if response:
-        cv2.imwrite("images/test.jpg", image)
+        cv2.imwrite("images/test.png", image)
     capture.release()
     return image
+
+def crop_face():
+    image = face_recognition.load_image_file("images/test.png")
+    face_locations = face_recognition.face_locations(image)
+    top, right, bottom, left = face_locations[0]
+    area = img.crop((left,top,right,bottom))
